de6278a23abd41df86dcb538f5b75c983c7dde85
SatishXceedance <satish.rajput@xceedance.com>
2018-03-23 03:11:28 -0700

    added configuration files

--- my_feature_requests/settings.py
+++ my_feature_requests/settings.py
@@ -39,6 +39,8 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_CREDENTIALS = True
 ROOT_URLCONF = 'my_feature_requests.urls'
 
 TEMPLATES = [
